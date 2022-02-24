d225334fbc10a468e8b3b14d70cd5050bc18e921
Caio Oliveira <caio.goncalves.pro@gmail.com>
2022-02-23 23:09:51 -0300

    Bugs and improvements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import type { NextPage } from "next";
+
 import Link from "next/link";
 import Head from "next/head";
 import Image from "next/image";
@@ -7,12 +8,16 @@ import Dropdown from "../components/dropdown";
 import Navbar from "../components/navbar";
 import styles from "../styles/Home.module.scss";
 import PictureOfMe from "../../public/images/[removal.ai]_tmp-61fbef991960f.png";
+import PictureOfMe2 from "../../public/images/[removal.ai]_tmp-6216dffde99f8.png";
 import site1 from "../../public/images/site1.png";
 import site2 from "../../public/images/site2.png";
 import site3 from "../../public/images/site3.png";
 import picture from "../../public/images/site4.png";
 import emoji from "../../public/images/waving-hand-sign_1f44b.png";
 import previewPicture from "../../public/images/android-chrome-512x512.png";
+
+const Aos = require("aos");
+import "aos/dist/aos.css";
 
 import {
   SiInstagram,
@@ -24,6 +29,10 @@ import {
 } from "react-icons/si";
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => {
@@ -91,7 +100,7 @@ const Home: NextPage = () => {
                   alt="hand waiving emoji"
                 />
               </div>
-              <h2>This is my personal website.</h2>
+              <h2>I am Caio and this is my personal website.</h2>
               <h2>
                 Here you can learn more about me, check some of my professional
                 work and get in touch.
@@ -99,8 +108,8 @@ const Home: NextPage = () => {
             </div>
             <div id="about-me" className={styles.section2}>
               <div className={styles.textContent}>
-                <h3>About me</h3>
-                <p>
+                <h3 data-aos="fade-right">About me</h3>
+                <p data-aos="fade-right">
                   Hi, my name is Caio Oliveira, I am 21 years old and a Computer
                   Engineering degree student at Universidade Federal de Goiás
                   (Goiania, GO - Brazil). I have been learning and working with
@@ -120,37 +129,44 @@ const Home: NextPage = () => {
                   Marvel), watch sports and learn new things every single day.
                 </p>
               </div>
-              <div className={styles.imgCC}>
+              <div data-aos="fade-up" className={styles.imgCC}>
                 <Image
                   width="410"
                   height="600"
                   src={PictureOfMe}
-                  alt="Picture of me"
+                  alt="Picture of Caio Oliveira"
                 />
               </div>
             </div>
             <div id="projects" className={styles.section3}>
-              <h3>Projects</h3>
+              <h3 data-aos="fade-in">Projects</h3>
               <div className={styles.content3}>
-                <div className={styles.Grid}>
-                  <h4>Tutty Motors Website</h4>
+                <div data-aos="fade-right" className={styles.Grid}>
+                  <h4>
+                    <strong>Tutty Motors Website</strong>
+                  </h4>
                   <div className={styles.imageContainer}>
                     <Link href="https://tuttymotors.vercel.app/" passHref>
                       <Image
                         width="900"
                         height="450px"
                         src={site1}
-                        alt="site1"
+                        alt="Picture of Tutty Site"
                       />
                     </Link>
                   </div>
                   <p>
-                    My first professional website made
-                    <br /> using Next, TS and Sass.
+                    This was my first professional website using front-end
+                    tecnologies. I teamed up with another collaborator at ELO Jr
+                    and we built it using NextJS, TS, Sass and Strapi. I was
+                    responsible for the front page, header, stocking and about
+                    pages.
                   </p>
                 </div>
-                <div className={styles.Grid}>
-                  <h4>Zupot Landing Page</h4>
+                <div data-aos="fade-up" className={styles.Grid}>
+                  <h4>
+                    <strong>Zupot Landing Page</strong>
+                  </h4>
                   <div className={styles.imageContainer}>
                     <Link
                       href="https://lp.zupot.com.br/lp-primeiro-ciclo"
@@ -160,37 +176,46 @@ const Home: NextPage = () => {
                         width="900"
                         height="450px"
                         src={site2}
-                        alt="site2"
+                        alt="Picture of Zupot Landing Page"
                       />
                     </Link>
                   </div>
                   <p>
-                    One of countless Landings Pages I have made <br /> at
-                    Kampana.
+                    This is one of countless Landings Pages I have made at
+                    Kampana. This one in particular was made for a tech company
+                    called Zupot in a first cicle strategy. At Kampana I was
+                    responsible for building all LP structure in RD Station and
+                    then use CSS to stylize it.
                   </p>
                 </div>
-                <div className={styles.Grid}>
-                  <h4>Tudaço Stores Page</h4>
+                <div data-aos="fade-left" className={styles.Grid}>
+                  <h4>
+                    <strong>Tudaço Stores Page</strong>
+                  </h4>
                   <div className={styles.imageContainer}>
                     <Link href="https://www.lojastudaco.com.br/lojas" passHref>
                       <Image
                         width="900"
                         height="450px"
                         src={picture}
-                        alt="site3"
+                        alt="Picture of Tudaco Stores Page"
                       />
                     </Link>
                   </div>
                   <p>
-                    Code maintenance in an e-commerce using <br />
-                    HTML, CSS and JS.
+                    This is a code maintenance I made for an e-commerce using
+                    HTML and CSS. The store is hosted at Tray but the platform
+                    accepts file uploading so I downloaded the old file and
+                    redesingned the whole page using the Google Maps iFrames.
                   </p>
                 </div>
               </div>
               <div id="contact" className={styles.section4}>
-                <h3>Contact</h3>
-                <h4>Here you can contact and find me in all my socials.</h4>
-                <div className={styles.iconsContainer}>
+                <h3 data-aos="fade-in">Contact</h3>
+                <h4 data-aos="fade-in">
+                  Here you can contact and find me in all my socials.
+                </h4>
+                <div data-aos="fade-in" className={styles.iconsContainer}>
                   <Link
                     href="https://www.linkedin.com/in/caio-oliveira-6889001bb/"
                     passHref
@@ -216,7 +241,7 @@ const Home: NextPage = () => {
                     />
                   </Link>
                 </div>
-                <div className={styles.iconsContainer}>
+                <div data-aos="fade-in" className={styles.iconsContainer}>
                   <Link
                     href="https://www.instagram.com/caio_goncalves_/"
                     passHref
@@ -246,7 +271,7 @@ const Home: NextPage = () => {
             </div>
           </div>
           <div className={styles.footer}>
-            <p>Developed by me. 2022.</p>
+            <p>Developed by Caio Oliveira. 2022.</p>
           </div>
         </body>
       </main>
